01454f31396fa38737eec84de561e27d9ca8e288
Alexandr Guzhva <alexanderguzhva@gmail.com>
2025-09-14 21:02:15 -0400

    cargo
    Signed-off-by: Alexandr Guzhva <alexanderguzhva@gmail.com>

--- step12/builder.sh
+++ step12/builder.sh
@@ -1,5 +1,7 @@
 export PATH=/usr/local/go/bin:$PATH
 
+source $HOME/.cargo/env
+
 LD_LIBRARY_PATH=/opt/gcc-15/lib64 \
 CMAKE_POLICY_VERSION_MINIMUM=3.5 CC=/opt/gcc-15/bin/gcc CXX=/opt/gcc-15/bin/g++ make
 
